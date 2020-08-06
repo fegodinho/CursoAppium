601d59718f4422011782e2217b58726b124d7a59
Felipe Godinho <fegodinho@gmail.com>
2020-08-06 13:07:42 -0300

    Aula 12 - Campo de Treinamento Appium

---
+++ src/main/java/br/sp/fegodinho/appium/FormularioTeste.java
@@ -0,0 +1,30 @@
+package br.sp.fegodinho.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class FormularioTeste {
+	
+	@Test
+	public void deveInstalarAPK() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    desiredCapabilities.setCapability("platformName", "Android");
+	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+	    desiredCapabilities.setCapability("automationName", "uiautomator2");
+	    //desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+	    //desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Felipe\\Desktop\\CursoAppium\\src\\main\\resources\\CTAppium_1_2.apk");	    
+	    	    
+	    AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+	    	    
+	    driver.quit();
+	}
+
+}
